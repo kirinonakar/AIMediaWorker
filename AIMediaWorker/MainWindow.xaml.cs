343a6752f4a91a3062dda11939ef72bc01ec1b18
kirinonakar <kirinonakar@gmail.com>
2026-08-25 15:59:56 +0900

    14

--- AIMediaWorker/MainWindow.xaml.cs
+++ AIMediaWorker/MainWindow.xaml.cs
@@ -1563,7 +1563,7 @@ public sealed partial class MainWindow : Window
         ToolTipService.SetToolTip(PositionSlider, F("TooltipPosition", Shortcut(ShortcutActions.PlayFromBeginning)));
         ToolTipService.SetToolTip(SubtitleList, F("TooltipSubtitleNavigation", Shortcut(ShortcutActions.PreviousSubtitle), Shortcut(ShortcutActions.NextSubtitle)));
         ToolTipService.SetToolTip(RepeatButton, L(_repeatMode switch { RepeatMode.One => "TooltipRepeatCurrent", RepeatMode.All => "TooltipRepeatPlaylist", _ => "TooltipRepeatOff" }));
-        ToolTipService.SetToolTip(CloseButton, L("TooltipClose"));
+        ToolTipService.SetToolTip(CloseButton, F("TooltipClose", Shortcut(ShortcutActions.CloseWindow)));
         UpdateFullscreenButton();
     }
 
